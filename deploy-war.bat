6e2fe4213c51fd2b6fff48cdcc2f3e5831a48946
FimiAnjara <anjaramandresiarisoamitia@gmail.com>
2025-10-17 15:26:31 +0300

    change to jakarta

--- deploy-war.bat
+++ deploy-war.bat
@@ -1,7 +1,7 @@
 @echo off
 set WAR_NAME=Sprint.war
 set SOURCE_DIR=
-set DEST_DIR=C:\xampp\tomcat\webapps
+set DEST_DIR=D:\apache-tomcat-10.1.28\webapps
 
 echo Copying %WAR_NAME% to %DEST_DIR% ...
 copy "%WAR_NAME%" "%DEST_DIR%\%WAR_NAME%" /Y

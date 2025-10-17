52a187f0ec35b71262c627465b447825ba8d901d
FimiAnjara <anjaramandresiarisoamitia@gmail.com>
2025-10-17 17:08:05 +0300

    test annotation

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,15 @@
-package main.java.app;
+import java.util.ResourceBundle.Control;
+import fw.Annotation;
 
 public class Main {
-    public static void main(String[] sed){
-        System.out.println("Hello word");
+    public static void main(String[] args) {
+        Class<Controller> controllerClass = Controller.class;
+        for (var method : controllerClass.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(Annotation.class)) {
+                Annotation annotation = method.getAnnotation(Annotation.class);
+                System.out.println("Méthode: " + method.getName() + ", URL: " + annotation.value());
+            }
+        }
+
     }
 }
